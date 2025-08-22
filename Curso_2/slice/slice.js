fb394c6fb7b4c9281e9b9f0df6aa708ff7afd4b4
Matuga09 <matischica@gmail.com>
2025-08-22 11:01:37 -0500

    Delete Curso_2 directory

--- Curso_2/slice/slice.js
+++
@@ -1,6 +0,0 @@
-const animal = ["ant", "bison", "camel", "duck", "elephant"]
-
-console.log(animal.slice(2))
-console.log(animal.slice(2, 4))
-console.log(animal.slice(-2))
-console.log(animal.slice())
