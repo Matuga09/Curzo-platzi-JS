d3d9353f58d87250a6cf050193f8ab334697a04d
Matuga09 <matischica@gmail.com>
2025-08-22 10:59:11 -0500

    Add files via upload

---
+++ Nivel avanzado/Curso_1/main.js
@@ -0,0 +1,19 @@
+const carro = {
+    marca: "Nissan",
+    modelo: "2022"
+}
+
+console.log(`Yo soy Matias Chica y tengo un carro, marca ${carro.marca} modelo ${carro.modelo}`);
+
+function restarDos(num){
+    return num-2;
+}
+function calcular(){
+    const sumartotal = 4+5;
+    return restarDos(sumartotal);
+}
+
+debugger;
+
+calcular();
+
